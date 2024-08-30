dbfd6caedaf24a34e763be0e3e8850065031f9fd
Ziyu Guo <61613867+ZiyuGuo99@users.noreply.github.com>
2024-08-30 09:51:18 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 DATASET="Objaverse"
 TYPE="point"
-SAMPLE_IDX=0
+SAMPLE_IDX=3
 PROMPT_IDX=0
 
 python3 main.py --prompt_type $TYPE --sample_idx $SAMPLE_IDX --prompt_idx $PROMPT_IDX --dataset $DATASET
+python3 gen_video.py --prompt_type $TYPE --sample_idx $SAMPLE_IDX --prompt_idx $PROMPT_IDX --dataset $DATASET
