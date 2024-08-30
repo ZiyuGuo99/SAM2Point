2c09a7b0a774c4dcc5fa3db7f519429684ba74e3
Ziyu Guo <61613867+ZiyuGuo99@users.noreply.github.com>
2024-08-30 09:50:19 +0800

    Update dataset.py

--- sam2point/dataset.py
+++ sam2point/dataset.py
@@ -6,6 +6,8 @@ import torch
 
 
 def load_S3DIS_sample(text_path, sample=False):
+    print("Loading point cloud from ", text_path)
+
     data = np.loadtxt(text_path)
     point, color = data[:, :3], data[:, 3:]
 
@@ -16,6 +18,7 @@ def load_S3DIS_sample(text_path, sample=False):
     return point, color
 
 def load_ScanNet_sample(data_path):
+    print("Loading point cloud from ", data_path)
     
     all_data = torch.load(data_path)
     
@@ -25,9 +28,11 @@ def load_ScanNet_sample(data_path):
     point = point - point.min(axis=0)
     point = point / point.max(axis=0)
     color = color / 255.
+
     return point, color
 
 def load_KITTI_sample(data_path, close=False):
+    print("Loading point cloud from ", data_path)
     all_data = np.load(data_path)
     
     point = all_data[:, :3]
@@ -37,23 +42,11 @@ def load_KITTI_sample(data_path, close=False):
     point = point - pmin
     pmax = point.max(axis=0)
     point = point / pmax
-    
-    # if close:
-    #     x_min, x_max = 0.3, 0.7
-    #     y_min, y_max = 0.3, 0.8
-
-    #     filter_mask = (point[:, 0] > x_min) & (point[:, 0] < x_max) & (point[:, 1] > y_min) & (point[:, 1] < y_max)
-    #     point = point[filter_mask]
-    #     color = color[filter_mask]
-
-    #     pmin = point.min(axis=0)
-    #     point = point - pmin
-    #     pmax = point.max(axis=0)
-    #     point = point / pmax
 
     return point, color
 
 def load_Objaverse_sample(data_path):
+    print("Loading point cloud from ", data_path)
     all_data = np.load(data_path)
     
     point = all_data[:, :3]
@@ -67,6 +60,7 @@ def load_Objaverse_sample(data_path):
     return point, color
 
 def load_Semantic3D_sample(data_path, id, sample=False):
+    print("Loading point cloud from ", data_path)
     all_data = np.load(data_path)
     
     point = all_data[:, :3]
@@ -89,10 +83,5 @@ def load_Semantic3D_sample(data_path, id, sample=False):
     point = point - pmin
     pmax = point.max(axis=0)
     point = point / pmax
-
-    # if sample: #for demo
-    #     indices = np.random.choice(point.shape[0], 600000, replace=False)
-    #     point = point[indices]
-    #     color = color[indices]
-
+    
     return point, color
