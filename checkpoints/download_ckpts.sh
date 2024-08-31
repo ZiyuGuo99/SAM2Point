06707de77d423b917a2b4e3c585fcf36040d7b7d
Ziyu Guo <61613867+ZiyuGuo99@users.noreply.github.com>
2024-08-31 02:48:58 -0700

    Update download_ckpts.sh

--- checkpoints/download_ckpts.sh
+++ checkpoints/download_ckpts.sh
@@ -9,23 +9,7 @@
 
 # Define the URLs for the checkpoints
 BASE_URL="https://dl.fbaipublicfiles.com/segment_anything_2/072824/"
-sam2_hiera_t_url="${BASE_URL}sam2_hiera_tiny.pt"
-sam2_hiera_s_url="${BASE_URL}sam2_hiera_small.pt"
-sam2_hiera_b_plus_url="${BASE_URL}sam2_hiera_base_plus.pt"
 sam2_hiera_l_url="${BASE_URL}sam2_hiera_large.pt"
-
-
-# Download each of the four checkpoints using wget
-echo "Downloading sam2_hiera_tiny.pt checkpoint..."
-wget $sam2_hiera_t_url || { echo "Failed to download checkpoint from $sam2_hiera_t_url"; exit 1; }
-
-echo "Downloading sam2_hiera_small.pt checkpoint..."
-wget $sam2_hiera_s_url || { echo "Failed to download checkpoint from $sam2_hiera_s_url"; exit 1; }
-
-echo "Downloading sam2_hiera_base_plus.pt checkpoint..."
-wget $sam2_hiera_b_plus_url || { echo "Failed to download checkpoint from $sam2_hiera_b_plus_url"; exit 1; }
 
 echo "Downloading sam2_hiera_large.pt checkpoint..."
 wget $sam2_hiera_l_url || { echo "Failed to download checkpoint from $sam2_hiera_l_url"; exit 1; }
-
-echo "All checkpoints are downloaded successfully."
