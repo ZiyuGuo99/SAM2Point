34ed37f8afd9fbf9ba9367cbbf79f8abf8aee679
Ziyu Guo <61613867+ZiyuGuo99@users.noreply.github.com>
2024-08-31 03:04:57 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-DATASET="Objaverse"
-TYPE="point"
-SAMPLE_IDX=3
-PROMPT_IDX=0
+DATASET="Objaverse"   # Dataset name
+TYPE="point"          # Prompt type - please choose from {'point', 'box', 'mask'}
+SAMPLE_IDX=0          # Sample ID of the given 3D examples
+PROMPT_IDX=0          # Prompt ID of the given prompt examples
 
 python3 main.py --prompt_type $TYPE --sample_idx $SAMPLE_IDX --prompt_idx $PROMPT_IDX --dataset $DATASET
 python3 gen_video.py --prompt_type $TYPE --sample_idx $SAMPLE_IDX --prompt_idx $PROMPT_IDX --dataset $DATASET
